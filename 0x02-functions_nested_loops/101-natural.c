e226f679ba739ab9974777ed623e7f1eeb6bbb3a
Nduduzo Blose <nduduzobb2@gmail.com>
2023-08-15 20:56:04 -0700

    Task 13"

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: integer
+ */
+int main(void)
+{
+	int i, sum;
+
+	i = 1;
+	sum = 0;
+	while (i < 1024)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum++;
+		}
+		i++;
+	}
+	return (sum);
+}
