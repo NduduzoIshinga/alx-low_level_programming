6a2d45b513b8171e41731e49df291711132eef9a
Nduduzo Blose <nduduzobb2@gmail.com>
2023-08-24 18:20:39 -0700

    Task 3

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses an array
+ * @a: array 1
+ * @n: num of elements in array
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int i, k, arr[] = {5, 6, 8, 9};
+
+	i = 0;
+	while (i < n)
+	{
+		arr[i] = a[n - i - 1];
+		i++;
+	}
+	k = 0;
+	while (k < n)
+	{
+		a[k] = arr[k];
+		k++;
+	}
+	a[k] = '\0';
+}
