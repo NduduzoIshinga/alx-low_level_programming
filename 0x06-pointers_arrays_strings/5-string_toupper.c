3e1cea60e90c6896f02cb1e61f9f0c45d08ee483
Nduduzo Blose <nduduzobb2@gmail.com>
2023-08-24 18:51:25 -0700

    task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *string_toupper - changes lowwer to upper case in a string
+ *@str: string
+ *Return: string
+ */
+char *string_toupper(char *str)
+{
+	int i;
+	char c;
+
+	i = 0;
+	while (i != '\0')
+	{
+		if (str[i] <= 122 && str[i] >= 97)
+		{
+			c = str[i];
+			str[i]= c - 32;
+		}
+		i++;
+	}
+	return (str);
+}
