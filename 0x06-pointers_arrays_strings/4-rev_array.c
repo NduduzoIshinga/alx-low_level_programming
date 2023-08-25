d09716ae2d5386046c8973d8619f17d7d95caaa2
Nduduzo Blose <nduduzobb2@gmail.com>
2023-08-24 18:31:40 -0700

    Task 4 fix

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,7 +8,7 @@
  */
 void reverse_array(int *a, int n)
 {
-	int i, k, arr[] = {5, 6, 8, 9};
+	int i, k, arr[] = {5, 6, 8, 9, 5, 24, 24, 356, 24, 24, 2, 24, 23, 5, 6, 8, 9, 2, 5};
 
 	i = 0;
 	while (i < n)
