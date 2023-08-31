85700bab42874abede9ec418a126a2b683442de4
Nduduzo Blose <nduduzobb2@gmail.com>
2023-08-30 18:05:20 -0700

    Task 1 fix

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * _puts_rev_recursion - puts mimic
+ * _print_rev_recursion - puts mimic
  * @s: string to be put
  * Return: void
  */
-void _puts_rev_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
 	{
 		return;
 	}
-	_puts_rev_recursion(s + 1);
+	_print_rev_recursion(s + 1);
 	_putchar(*s);
 }
